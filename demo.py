cfa216815fdc68b1b3980bfe2aa863de8ea43b6f
Chui yi Liu <chuiyiliu@Chuis-MacBook-Pro.local>
2021-07-22 11:03:22 -0700

    Add quick demo with examples

--- demo.py
+++ demo.py
@@ -9,9 +9,8 @@ import util
 
 
 #TODO modify as user input
-hint_ab_path = './imgs/migrant_mother/im_ab.npy'
-hint_mask_path = './imgs/migrant_mother/im_mask.npy'
-base_path = './'
+hint_ab_path = './imgs/ppr10k_1019/meanshift_im_ab.npy'
+hint_mask_path = './imgs/ppr10k_1019/meanshift_im_mask.npy'
 model_path = './weights/caffemodel_mask01_rec.pth'
 
 use_gpu = False
@@ -19,26 +18,32 @@ l_cent = 50
 l_norm =  100
 ab_norm = 110
 mask_cent = 0
-H_proc, W_proc = 256, 256
+H_proc, W_proc = 224, 224
 
 colorizer = models.siggraph.SIGGRAPHGenerator()  # load the model
 a = torch.load(model_path)
 colorizer.load_state_dict(a,strict=False)
 
+def color_sampling():
+    """
+    Return mask and ab channel mask in
+    """
+    raise NotImplementedError
+    
 
-def run_color_matching(source_image):
+def run_color_matching(source_image, reference_image):
     # convert it to npy
     if(hint_ab_path is None):
         in_ab = np.zeros((H_proc, W_proc, 2))
     else:
-        in_ab = np.load(hint_ab_path).transpose((1,2,0)) # ./imgs/migrant_mother/im_ab.npy
-        # in_ab = np.load(hint_ab_path)
+        # in_ab = np.load(hint_ab_path).transpose((1,2,0)) # ./imgs/migrant_mother/im_ab.npy
+        in_ab = np.load(hint_ab_path)
 
     if(hint_mask_path is None):
         in_mask = np.zeros((H_proc, W_proc, 1))
     else:
-        in_mask = 1.*np.load(hint_mask_path).transpose((1,2,0)) # ./imgs/migrant_mother/im_mask.npy
-        # in_mask = 1.*np.load(hint_mask_path)
+        # in_mask = 1.*np.load(hint_mask_path).transpose((1,2,0)) # ./imgs/migrant_mother/im_mask.npy
+        in_mask = 1.*np.load(hint_mask_path)
 
     print('[INFO] Initializing colorization model...')
     a = torch.load(model_path)
@@ -49,6 +54,8 @@ def run_color_matching(source_image):
     colorizer.eval()
 
     (H_orig,W_orig) = source_image.shape[:2]
+    gray = cv2.cvtColor(source_image, cv2.COLOR_BGR2GRAY)
+    source_image = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
 
     # resize to processing size, take L channel for input
     img_rs = cv2.resize(source_image, (W_proc, H_proc), interpolation=cv2.INTER_CUBIC)
@@ -80,8 +87,12 @@ def run_color_matching(source_image):
 
 iface = gr.Interface(
     run_color_matching, 
-    gr.inputs.Image(shape=(256, 256)),
+    [gr.inputs.Image(shape=(224, 224)),
+    gr.inputs.Image(shape=(2224, 224))],
     "image",
+    examples=[
+        ["imgs/ppr10k_1019/source.png", "imgs/ppr10k_1019/reference.png"]
+    ]
     )
 
 if __name__ == "__main__":
